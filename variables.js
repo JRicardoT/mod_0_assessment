0527c0107fb7f7adb40e7cf1d0cb455d61c0c6d3
jose Tovar <jose.tovar102397@gmail.com>
2021-07-30 14:24:09 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,16 @@
+var animal = "lion";
+animal = "penguin";
+console.log(animal)
+var isCuddly = false;
+console.log(isCuddly);
+var num8 = 5;
+console.log(num8);
+var num3 = 20;
+console.log(num3);
+var quotient = num3 / num8;
+console.log(quotient);
+var favoriteFilms = ["The lion king","into the spiderverse","Maze runner"];
+favoriteFilms.shift();
+console.log(favoriteFilms);
+animal.length
+console.log(animal.length)
