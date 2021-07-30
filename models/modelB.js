f5b05fb1d76ee301ec99f8911c1b756b42e4b34f
jose Tovar <jose.tovar102397@gmail.com>
2021-07-30 14:42:05 -0600

    Add house object

---
+++ models/modelB.js
@@ -0,0 +1,12 @@
+var house = {
+
+  typeOfHouse: "Townhome",
+  frontYard: "All concrete",
+  numberOfRooms: 4,
+  numberOfBathrooms: 3,
+  hasWaterHeater: true,
+  isOld: false,
+  livingRoomFurniture: ["Couch","TV","Coffe table"],
+  kitchenAppliances: ["Stove","Dishwasher","Fridge"],
+  
+}
