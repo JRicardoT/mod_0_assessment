f169e3ebe0bb494933b00f08df9f0ebf8c4da457
jose Tovar <jose.tovar102397@gmail.com>
2021-07-30 14:35:21 -0600

    Add laptop object

---
+++ models/modelA.js
@@ -0,0 +1,12 @@
+var laptop = {
+
+  brand: "Apple",
+  name: "Macbook pro",
+  numberOfPorts: 2,
+  numberOfCameras: 1,
+  isBig: false,
+  keyboard: true,
+  homePrograms: ["safari","facetime","apple store"],
+  entertainmentApps: ["apple music","apple tv"]
+
+}
